6cf7ab9f711856928c46a40eeac8d6a9645394c1
antonBlagodarnyy <antonbgi98@gmail.com>
2025-09-19 12:36:26 +0200

    added openlayers map and startyed marker

---
+++ ChatMapFront/src/app/Components/map/marker/marker.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marker',
+  imports: [],
+  templateUrl: './marker.component.html',
+  styleUrl: './marker.component.css'
+})
+export class MarkerComponent {
+
+}
--- ChatMapFront/src/app/Pages/map/map.component.ts
+++ ChatMapFront/src/app/Pages/map/map.component.ts
@@ -1,17 +1,30 @@
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../Components/map/header/header.component';
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
 
 @Component({
   selector: 'app-map',
-  imports: [
-    HeaderComponent,
-  ],
-  templateUrl: './map.component.html',
+  imports: [HeaderComponent],
+  templateUrl: './map.component.html'
 })
-export class MapComponent {
- 
-  constructor(
-  ) {}
+export class MapComponent implements OnInit {
+  map?: Map;
 
- 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.map = new Map({
+    
+      layers: [new TileLayer({ source: new OSM({}) })],
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+      target: 'map',
+    });
+    console.log(this.map.getControls())
+  }
 }
