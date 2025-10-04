214862787a2b6686fb2665309ad86253a833d3ea
antonBlagodarnyy <antonbgi98@gmail.com>
2025-10-04 12:29:20 +0200

    Added guard for already logged in

---
+++ ChatMapFront/src/app/Guards/already-auth.guard.ts
@@ -0,0 +1,20 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../Services/auth.service';
+import { map, take } from 'rxjs';
+import { inject } from '@angular/core';
+
+export const alreadyAuthGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+     return authService.user.pipe(
+        take(1),
+        map((user) => {
+          if (user) {
+            return router.createUrlTree(['/map']);;
+          } else {
+            return true;
+          }
+        })
+      );
+};
--- ChatMapFront/src/app/app.routes.ts
+++ ChatMapFront/src/app/app.routes.ts
@@ -3,15 +3,17 @@ import { LoginComponent } from './Components/auth/login/login.component';
 import { SignupComponent } from './Components/auth/signup/signup.component';
 import { WelcomeComponent } from './Pages/welcome/welcome.component';
 import { MapComponent } from './Pages/map/map.component';
-import { AuthGuard } from './Guards/auth-guard';
+import { NoAuthGuard } from './Guards/no-auth.guard';
 import { ChatComponent } from './Pages/chat/chat.component';
 import { LocationComponent } from './Pages/location/location.component';
 import { locationGuard } from './Guards/location.guard';
+import { alreadyAuthGuard } from './Guards/already-auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
     component: WelcomeComponent,
+    canActivate: [alreadyAuthGuard],
   },
   {
     path: 'auth',
@@ -19,16 +21,21 @@ export const routes: Routes = [
       { path: 'login', component: LoginComponent },
       { path: 'signup', component: SignupComponent },
     ],
+    canActivate: [alreadyAuthGuard],
   },
-  { path: 'location', component: LocationComponent, canActivate: [AuthGuard] },
+  {
+    path: 'location',
+    component: LocationComponent,
+    canActivate: [NoAuthGuard],
+  },
   {
     path: 'map',
     component: MapComponent,
-    canActivate: [AuthGuard, locationGuard],
+    canActivate: [NoAuthGuard, locationGuard],
   },
   {
     path: 'chat',
     component: ChatComponent,
-    canActivate: [AuthGuard, locationGuard],
+    canActivate: [NoAuthGuard, locationGuard],
   },
 ];
