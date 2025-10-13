efba3bc762328ca2fdb9aeda515acab972a3e478
antonBlagodarnyy <antonbgi98@gmail.com>
2025-10-13 13:13:39 +0200

    Location and friend ms starter

---
+++ Friend/src/test/java/com/ChatMap/Friend/FriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChatMap.Friend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Location/src/main/java/com/ChatMap/Location/Filters/ValidateJwtFilter.java
@@ -0,0 +1,46 @@
+package com.ChatMap.Location.Filters;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.ChatMap.Location.Services.JwtService;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ValidateJwtFilter extends OncePerRequestFilter {
+
+	@Autowired
+	private JwtService jwtService;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+
+		final String authorizationHeader = request.getHeader("Authorization");
+
+		String jwt = null;
+		DecodedJWT decodedJwt = null;
+
+		if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+			jwt = authorizationHeader.substring(7);
+			decodedJwt = jwtService.decodeJwt(jwt);
+		}
+		String userId = decodedJwt.getSubject();
+
+		UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userId, null,
+				List.of());
+
+		SecurityContextHolder.getContext().setAuthentication(authToken);
+		filterChain.doFilter(request, response);
+	}
+
+}
