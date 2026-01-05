ad422af8afd36463696e8006b1a9859a5ce29f11
antonBlagodarnyy <antonbgi98@gmail.com>
2026-01-05 09:38:36 +0100

    small correction

--- Client/src/environments/environment.ts
+++ Client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://chatmap.duckdns.org/api',
-  wsUrl: 'wss://chatmap.duckdns.org/ws',
+  apiUrl: 'https://chatmap.duckdns.org/api/',
+  wsUrl: 'wss://chatmap.duckdns.org/ws/',
 };
